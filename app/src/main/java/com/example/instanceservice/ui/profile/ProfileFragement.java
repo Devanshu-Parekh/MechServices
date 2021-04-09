6f84a7aa8edb7c718195f0a43092d15b8a978f13
Devanshu-Parekh <devanshuparekh22@gmail.com>
2021-04-09 13:43:32 +0530

    First commit pushing project on github

---
+++ app/src/main/java/com/example/instanceservice/ui/profile/ProfileFragement.java
@@ -0,0 +1,38 @@
+package com.example.instanceservice.ui.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.instanceservice.R;
+
+public class ProfileFragement extends Fragment {
+
+    private ProfileViewModel mViewModel;
+
+    public static ProfileFragement newInstance() {
+        return new ProfileFragement();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
